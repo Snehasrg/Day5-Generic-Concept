9b7dad559aa26f1bea1fa6c15fd13689dc7f1713
Snehasrg <sghongade642@gmail.com>
2023-03-25 00:21:09 +0530

    Method Calling

--- Program.cs
+++ Program.cs
@@ -7,7 +7,9 @@ namespace GenericConcept
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Generic Concepts Problems");
-            Console.WriteLine("Select any one option \n1.Delete Integer,double,character element from array.\n2.Generic Method to Delete Integer,double,character element from array.");
+            Console.WriteLine("Select any one option:- \n1.Delete Integer,double,character Element from array." +
+                              "\n2.Generic Method to Delete Integer,double,character Element from array." +
+                              "\n3.Generic Class to Delete Integer,double,character Element from array.");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -29,21 +31,41 @@ namespace GenericConcept
                     DeleteArrayElement.DeleteCharElement(charArry, charDelete);
                     break;
                 case 2:
-                    int[] intArryG = DeleteArrayElement.GetIntElement();
+                    int[] intArryG = GenericMethod.GetIntElement();
                     Console.WriteLine("Plese pick up any one elemnt");
                     int deleteG = Convert.ToInt32(Console.ReadLine());
                     GenericMethod.DeleteElementFromArray<int>(intArryG, deleteG);
                  
-                    double[] doubleArryG = DeleteArrayElement.GetDoubleElement();
+                    double[] doubleArryG = GenericMethod.GetDoubleElement();
                     Console.WriteLine("Plese pick up any one elemnt");
                     double doubleDeleteG = Convert.ToDouble(Console.ReadLine());
                     GenericMethod.DeleteElementFromArray<double>(doubleArryG, doubleDeleteG);
                     
-                    char[] charArryG = DeleteArrayElement.GetCharElement();
+                    char[] charArryG = GenericMethod.GetCharElement();
                     Console.WriteLine("Plese pick up any one elemnt");
                     char charDeleteG = Convert.ToChar(Console.ReadLine());
                     GenericMethod.DeleteElementFromArray<char>(charArryG, charDeleteG);
                     break;
+                case 3:
+                    int[] intArryGenericClass = GenericMethod.GetIntElement();
+                    Console.WriteLine("Plese pick up any one elemnt");
+                    int deleteGenericClass = Convert.ToInt32(Console.ReadLine());
+                    GenericClass<int> deleteElementGeneric = new GenericClass<int>(intArryGenericClass, deleteGenericClass);
+                    deleteElementGeneric.RemoveElementFromGenericClass();
+
+                    double[] doubleArryGenericClass = GenericMethod.GetDoubleElement();
+                    Console.WriteLine("Plese pick up any one elemnt");
+                    double doubleDeleteGenericClass = Convert.ToDouble(Console.ReadLine());
+                    GenericClass<double> deleteElementGeneric1 = new GenericClass<double>(doubleArryGenericClass, doubleDeleteGenericClass);
+                    deleteElementGeneric1.RemoveElementFromGenericClass();
+                    
+                    char[] charArryGenericClass = GenericMethod.GetCharElement();
+                    Console.WriteLine("Plese pick up any one elemnt");
+                    char charDeleteGenericClass = Convert.ToChar(Console.ReadLine());
+                    GenericClass<char> deleteElementGeneric2 = new GenericClass<char>(charArryGenericClass, charDeleteGenericClass);
+                    deleteElementGeneric2.RemoveElementFromGenericClass();
+                    break;
+
 
                 default:
                     Console.WriteLine("Please Enter Right Option number From given List");
